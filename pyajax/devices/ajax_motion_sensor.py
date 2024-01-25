5c0c856c390256fd24250ba8bc8aaa429ecb6d4b
Danny Mauro <63847311+Den901@users.noreply.github.com>
2024-01-25 11:17:17 +0100

    Update ajax_motion_sensor.py

--- pyajax/devices/ajax_motion_sensor.py
+++ pyajax/devices/ajax_motion_sensor.py
@@ -1,1 +1,22 @@
 # FEATURES TO DEVELOP: MOTION STATUS, TEMPERATURE SENSOR 
+
+
+#API_DATA
+MotionProtect{
+indicatorLightMode	IndicatorLightMode[...]
+indicatorLightModeSupported	boolean
+readOnly: true
+Indicates whether device firmware supports the indicatorLightMode setting. If false, only STANDARD mode is allowed. Hub firmware of version >=2.8.103 is also required for settings to have any effect, otherwise device will behave as if light indicator mode STANDARD was selected.
+
+alwaysActive*	boolean
+sensitivity*	integer($int32)
+minimum: 0
+maximum: 2
+sirenTriggers*	[string
+Enum:
+[ MOTION ]
+]
+deviceType*	string
+Enum:
+[ MotionProtect ]
+}
